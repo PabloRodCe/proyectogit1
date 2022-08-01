5e6dbbef4900c96b47c33d43aa366d0ca86323ac
Pablo Rod <Pablo Rod@DESKTOP-PAS83AE>
2022-08-01 14:58:02 -0600

    se agregan los metodos de acceso get and set

--- src/proyectogit1/persona.java
+++ src/proyectogit1/persona.java
@@ -12,5 +12,31 @@ public class persona {
     private int codigo;
     private String nombre;
     private String dirrecion;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDirrecion() {
+        return dirrecion;
+    }
+
+    public void setDirrecion(String dirrecion) {
+        this.dirrecion = dirrecion;
+    }
+    
+    
     
 }
